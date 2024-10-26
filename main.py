f81721bf46e4c577a39eb27f267c41da8a3303de
Matvey <@matvej17071987>
2024-10-26 14:17:21 +0300

    Commit main.py

--- main.py
+++ main.py
@@ -1,9 +1,44 @@
-class Student:
+# class Student:
+# #
+# #
+# #     def __init__(self):
+# #         self.height = 160  #конструктор, инициализация полей !!        self.height = 160   #self - посилання на самого себе !!
+# #         print("I am alive!")
+# #
+# #
+# # first_student = Student()
 
 
-    def __init__(self):
-        self.height = 160  #конструктор, инициализация полей !!        self.height = 160   #self - посилання на самого себе !!
-        print("I am alive!")
+# class Student:
+#     def __init__(self, height=160):  # параметр по умолчанию
+#         self.height = height
+#
+#
+# nick = Student(height=160)
+# kate = Student(height=170)
+# print(nick.height)
+# print(kate.height)
+
+# class Student:
+#
+#     def __init__(self):
+#         self.height = 170
+#
+#     def printer(self):
+#             print(self.height)
+#
+# student1 = Student()
+# student1.printer()
+
+class Car:
+
+    def __init__(self, wheels, frame):
+        self.wheels = wheels
+        self.frame = frame
 
 
-first_student = Student()
+car = Car(4, "metal")
+
+
+print(f"{car.wheels} wheels")
+print(f"{car.frame} frame")
