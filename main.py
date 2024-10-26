81050939b6bfdecd2a80e448aa188dc39e731d98
Matvey <@matvej17071987>
2024-10-26 13:14:07 +0300

    Commit main.py

---
+++ main.py
@@ -0,0 +1,9 @@
+class Student:
+
+
+    def __init__(self):
+        self.height = 160  #конструктор, инициализация полей !!        self.height = 160   #self - посилання на самого себе !!
+        print("I am alive!")
+
+
+first_student = Student()
